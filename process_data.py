07349d65cd8cf2b018e5f3b2fad30ec744218f4e
Mauricio Hess <mauricio.leao.hess@gmail.com>
2021-12-30 20:08:43 +0100

    First Warning adapted

--- process_data.py
+++ process_data.py
@@ -1,14 +1,10 @@
 """
 Preprocessing of Data
 Project: Disaster Response Pipeline (Udacity - Data Science Nanodegree)
-
 Sample Script Syntax:
-
 > python process_data.py <path to messages csv file> <path to categories csv file> <path to sqllite  destination db>
-
 Sample Script Execution:
 > python process_data.py disaster_messages.csv disaster_categories.csv disaster_response_db.db
-
 Arguments Description:
     1) Path to the CSV file containing messages (e.g. disaster_messages.csv)
     2) Path to the CSV file containing categories (e.g. disaster_categories.csv)
@@ -114,6 +110,39 @@ Arguments Description: \n\
 1) Path to the CSV file containing messages (e.g. disaster_messages.csv)\n\
 2) Path to the CSV file containing categories (e.g. disaster_categories.csv)\n\
 3) Path to SQLite destination database (e.g. disaster_response_db.db)")
+
+if __name__ == '__main__':
+    main()
+
+def save_data(df, database_filename):
+    pass  
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
 
 if __name__ == '__main__':
     main()
