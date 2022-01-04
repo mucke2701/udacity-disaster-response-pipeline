d5830d19e95cbdd39ec7bc927850b421e16c4f62
Mauricio Hess <mauricio.leao.hess@gmail.com>
2022-01-05 00:04:49 +0100

    process data aktualisiert

--- process_data.py
+++ process_data.py
@@ -11,7 +11,7 @@ Arguments Description:
     3) Path to SQLite destination database (e.g. disaster_response_db.db)
 """
 
-# Import all the relevant libraries
+# Import all the relevant librariest
 import sys
 import numpy as np
 import pandas as pd
@@ -114,35 +114,3 @@ Arguments Description: \n\
 if __name__ == '__main__':
     main()
 
-def save_data(df, database_filename):
-    pass  
-
-
-def main():
-    if len(sys.argv) == 4:
-
-        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
-
-        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
-              .format(messages_filepath, categories_filepath))
-        df = load_data(messages_filepath, categories_filepath)
-
-        print('Cleaning data...')
-        df = clean_data(df)
-        
-        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
-        save_data(df, database_filepath)
-        
-        print('Cleaned data saved to database!')
-    
-    else:
-        print('Please provide the filepaths of the messages and categories '\
-              'datasets as the first and second argument respectively, as '\
-              'well as the filepath of the database to save the cleaned data '\
-              'to as the third argument. \n\nExample: python process_data.py '\
-              'disaster_messages.csv disaster_categories.csv '\
-              'DisasterResponse.db')
-
-
-if __name__ == '__main__':
-    main()
